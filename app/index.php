b4dfd887bc5e93c047b5cb87017390ae4d58d150
Nikita Nyatin <yesnik@yandex.ru>
2021-03-28 13:14:19 +0500

    Add mysql

--- app/index.php
+++ app/index.php
@@ -1,3 +1,20 @@
 <?php
 
-phpinfo();
+$host = 'db';
+$user = 'root';
+$password = 'root';
+
+try {
+  $db = new PDO("mysql:host=$host;dbname=information_schema", $user, $password);
+  
+  $res = $db->query('SELECT * FROM global_status');
+  
+  echo '<pre>';
+  while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
+
+    print_r($row);
+  }
+
+} catch (PDOException $e) {
+  echo $e->getMessage();
+}
